c0a6797206022ab5fc8849c6a02a70ce72b90208
mudrakpatel <mudrakpatel96@gmail.com>
2017-01-30 17:28:32 -0500

    Ex4 half done

---
+++ MudrakPatel_Lab03_Ex4/MyEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MudrakPatel_Lab03_Ex4
+{
+    public delegate void MyEventHandler(string username);
+    class MyEvent
+    {
+        public static void showUserName(string username)
+        {
+            MessageBox.Show("Welcome "+username,"Greetings :D");
+        }
+    }
+}
