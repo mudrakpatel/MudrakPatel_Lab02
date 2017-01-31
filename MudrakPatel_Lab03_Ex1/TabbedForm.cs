6f2a8defe4e7b1a190e573dcf4bc206351cccf2d
mudrakpatel <mudrakpatel96@gmail.com>
2017-01-31 16:20:23 -0500

    Ex1 Almost done :D

--- MudrakPatel_Lab03_Ex1/TabbedForm.cs
+++ MudrakPatel_Lab03_Ex1/TabbedForm.cs
@@ -19,7 +19,66 @@ namespace MudrakPatel_Lab03_Ex1
 
         private void checkedListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //if (checkedListBox.GetItemCheckState(0) == CheckState.Checked){
+            //    this.Refresh();
+            //    textBox1.Show();
+            //    linkLabel1.Show();
+            //}
+            //else if (checkedListBox.GetItemCheckState(1) == CheckState.Checked){
+            //    this.Refresh();
+            //    textBox2.Show();
+            //    linkLabel2.Show();
+            //}
+            //else if (checkedListBox.GetItemCheckState(2) == CheckState.Checked){
+            //    this.Refresh();
+            //    textBox3.Show();
+            //    linkLabel3.Show();
+            //}
+            //else if (checkedListBox.GetItemCheckState(3) == CheckState.Checked){
+            //    this.Refresh();
+            //    textBox4.Show();
+            //    linkLabel4.Show();
+            //}
+            //else if (checkedListBox.GetItemCheckState(4) == CheckState.Checked){
+            //    this.Refresh();
+            //    textBox5.Show();
+            //    linkLabel5.Show();
+            //}
+            //else if (checkedListBox.GetItemCheckState(5) == CheckState.Checked){
+            //    this.Refresh();
+            //    textBox6.Show();
+            //    linkLabel6.Show();
+            //}
+            //else{
+            //    textBox1.Hide();
+            //    linkLabel1.Hide();
+            //    textBox2.Hide();
+            //    linkLabel2.Hide();
+            //    textBox3.Hide();
+            //    linkLabel3.Hide();
+            //    textBox4.Hide();
+            //    linkLabel4.Hide();
+            //    textBox5.Hide();
+            //    linkLabel5.Hide();
+            //    textBox6.Hide();
+            //    linkLabel6.Hide();
+            //}
 
+            switch (checkedListBox.SelectedItem.ToString())
+            {
+                case "Link1": textBox1.Show();
+                              linkLabel1.Show();
+                    break;
+                case "Link2": textBox2.Show();
+                              linkLabel2.Show();
+                    break;
+                case "Link3": textBox3.Show();
+                              linkLabel3.Show();
+                    break;
+                case "Link4": textBox4.Show();
+                    linkLabel4.Show();
+                    break;
+            }
         }
     }
 }
